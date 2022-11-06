8e9191234a705f22e113e1bc0f69a43f53362260
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-06 17:31:09 +0000

    Updating Code ...

--- Functions/decodingSimulation2D.m
+++ Functions/decodingSimulation2D.m
@@ -1,111 +1,119 @@
-function [inputMatrix, probabilityMatrix] = decodingSimulation2D(levelCounter, bitLength, symbolDuration, noisedSignal, normal)
+function [inputMatrix, probabilityMatrix, dataPoints] = decodingSimulation2D(levelCounter, bitLength, symbolDuration, noisedSignal, normal, randomBits1, randomBits2)
     
-           % Generating discrete received values from the transmission
+    dataPoints          = zeros(1, bitLength);
+    Decoded             = zeros(1, bitLength);                               % Decoded
+    Cecoded             = zeros(1, bitLength);        
+    inputAlphabet       = zeros(levelCounter, levelCounter);       % Input Alph.
+    Alphabet            = zeros(levelCounter, levelCounter);       % Alpabet    
+    inputMatrix         = zeros(0 + 1, levelCounter^2 + 1);
+    probabilityMatrix   = zeros(levelCounter^2 + 1, levelCounter^2 + 1);
 
-        for message_operator = 2:1:bitLength*symbolDuration
-                    Rn(message_operator) = noisedSignal(symbolDuration*message_operator)*normal + (levelCounter + 1)*(1 + 1i); 
+    
+    % Generating discrete received values from the transmission
+
+    for message_operator = 2:1:bitLength
+                dataPoints(message_operator) = ...
+                    noisedSignal(symbolDuration*message_operator)*normal...
+                  + complex(levelCounter + 1, levelCounter + 1); 
+    end
+
+    % Generation of the symbol values for use in communications
+
+    for b = 1:1:levelCounter^2 % Number of symbol values       
+        Alphabet(b) = b - 1;        
+    end
+
+    % Naming of the symbol for each constellation values
+
+    alphabetCounter = 1;
+
+    for b = 1:1:levelCounter % In-phase value        
+        for a = 1:1:levelCounter  % Out-phase value          
+            inputAlphabet(b,a) = Alphabet(alphabetCounter);            
+            alphabetCounter = alphabetCounter + 1;
         end
+    end
 
-        % Generation of the symbol values for use in communications
+    % Decoded Values
 
-        for a = 1:1:levelCounter^2 % Number of symbol values       
-            Alphabet(a) = a - 1;        
-        end
-
-        % Naming of the symbol for each constellation values
-
-        alpha_counter = 1;
-
-        for a = 1:1:levelCounter % In-phase value        
-            for b = 1:1:levelCounter  % Out-phase value          
-                Input_Alphabet(a,b) = Alphabet(alpha_counter);            
-                alpha_counter = alpha_counter + 1;
-            end
-        end
-
-        Rp = Prime_seed_1 + 1j*Prime_seed_2;
-
-        % Decoded Values
-
-        Rn_re = real(Rn); % real
-        Rn_im = imag(Rn); % imaginary
+    dataPointsReal = real(dataPoints); % real
+    dataPointsImag = imag(dataPoints); % imaginary
 
 
-        for dec = 2:1:bitLength    
-            for a = 1:1:levelCounter   
-                for b = 1:1:levelCounter 
-                    if Rn_re(dec) < 3 && ...
-                       Rn_im(dec) < 3
-                        Decoded(dec) = Input_Alphabet(1,1);
-                    elseif Rn_re(dec) > 2*levelCounter - 1 && ...
-                           Rn_im(dec) > 2*levelCounter - 1
-                        Decoded(dec) = ...
-                            Input_Alphabet(levelCounter  ,levelCounter  );
-                    elseif Rn_re(dec) > 2*levelCounter - 1 && ...
-                           Rn_im(dec) < 3
-                        Decoded(dec) = ...
-                            Input_Alphabet(levelCounter  ,1);
-                    elseif Rn_re(dec) < 3 && ...
-                           Rn_im(dec) > 2*levelCounter - 1
-                        Decoded(dec) = ...
-                            Input_Alphabet(1  ,levelCounter);
-                    elseif Rn_re(dec) > 2*a - 1 && ...
-                           Rn_re(dec) < 2*a + 1 && ...
-                           Rn_im(dec) > 2*levelCounter + 1
-                        Decoded(dec) = ...
-                            Input_Alphabet(a  ,levelCounter);
-                    elseif Rn_re(dec) > 2*a - 1 && ...
-                           Rn_re(dec) < 2*a + 1 && ...
-                           Rn_im(dec) < 3
-                        Decoded(dec) = ...
-                            Input_Alphabet(a  ,1);
-                    elseif Rn_im(dec) > 2*b - 1 && ...
-                           Rn_im(dec) < 2*b + 1 && ...
-                           Rn_re(dec) > 2*levelCounter + 1
-                        Decoded(dec) = ...
-                            Input_Alphabet(levelCounter  ,b);
-                    elseif Rn_im(dec) > 2*b - 1 && ...
-                           Rn_im(dec) < 2*b + 1 && ...
-                           Rn_re(dec) < 3
-                        Decoded(dec) = Input_Alphabet(1  ,b); 
-                    elseif Rn_re(dec) > 2*a-1 && ...
-                           Rn_im(dec) > 2*b-1 && ...
-                           Rn_re(dec) < 2*a+1 && ...
-                           Rn_im(dec) < 2*b+1 
-                        Decoded(dec) = Input_Alphabet(a,b);                 
-                    end
-                end
-            end
-
-             for a = 1:1:levelCounter
-                for b = 1:1:levelCounter
-                    if  Prime_seed_1(dec) >= (2*a ...
-                            - 1) ...
-                     && Prime_seed_1(dec) <= (2*a ...
-                            + 1) ...
-                     && Prime_seed_2(dec) >= (2*b ...
-                            - 1) ...
-                     && Prime_seed_2(dec) <= (2*b ...
-                            + 1)                    
-                            Cecoded(dec) = Input_Alphabet(a,b);
-                    end               
-                end
-             end        
-        end     % decision algorithm
-
-        for c = 1:1:length(Decoded)     
-            for a = 1:1:levelCounter^2 + 1
-
-                if Cecoded(c) == (a - 1)    
-                    inputMatrix(a) = inputMatrix(a) + 1; 
-                end
-
-                for b = 1:1:levelCounter^2 + 1          
-                    if Decoded(c) == (a - 1) && Cecoded(c) == (b - 1)                    
-                        probabilityMatrix(a,b) = probabilityMatrix(a,b) + 1;
-                    end
+    for c = 2:1:bitLength    
+        for b = 1:1:levelCounter   
+            for a = 1:1:levelCounter 
+                if dataPointsReal(c) < 3 && ...
+                   dataPointsImag(c) < 3
+                    Decoded(c) = inputAlphabet(1,1);
+                elseif dataPointsReal(c) > 2*levelCounter - 1 && ...
+                       dataPointsImag(c) > 2*levelCounter - 1
+                    Decoded(c) = ...
+                        inputAlphabet(levelCounter, levelCounter);
+                elseif dataPointsReal(c) > 2*levelCounter - 1 && ...
+                       dataPointsImag(c) < 3
+                    Decoded(c) = ...
+                        inputAlphabet(levelCounter, 1);
+                elseif dataPointsReal(c) < 3 && ...
+                       dataPointsImag(c) > 2*levelCounter - 1
+                    Decoded(c) = ...
+                        inputAlphabet(1, levelCounter);
+                elseif dataPointsReal(c) > 2*b - 1 && ...
+                       dataPointsReal(c) < 2*b + 1 && ...
+                       dataPointsImag(c) > 2*levelCounter + 1
+                    Decoded(c) = ...
+                        inputAlphabet(b ,levelCounter);
+                elseif dataPointsReal(c) > 2*b - 1 && ...
+                       dataPointsReal(c) < 2*b + 1 && ...
+                       dataPointsImag(c) < 3
+                    Decoded(c) = ...
+                        inputAlphabet(b ,1);
+                elseif dataPointsImag(c) > 2*a - 1 && ...
+                       dataPointsImag(c) < 2*a + 1 && ...
+                       dataPointsReal(c) > 2*levelCounter + 1
+                    Decoded(c) = ...
+                        inputAlphabet(levelCounter, a);
+                elseif dataPointsImag(c) > 2*a - 1 && ...
+                       dataPointsImag(c) < 2*a + 1 && ...
+                       dataPointsReal(c) < 3
+                    Decoded(c) = inputAlphabet(1, a); 
+                elseif dataPointsReal(c) > 2*b-1 && ...
+                       dataPointsImag(c) > 2*a-1 && ...
+                       dataPointsReal(c) < 2*b+1 && ...
+                       dataPointsImag(c) < 2*a+1 
+                    Decoded(c) = inputAlphabet(b,a);                 
                 end
             end
         end
+
+        for b = 1:1:levelCounter
+            for a = 1:1:levelCounter
+                if  randomBits1(c) >= (2*b ...
+                        - 1) ...
+                 && randomBits1(c) <= (2*b ...
+                        + 1) ...
+                 && randomBits2(c) >= (2*a ...
+                        - 1) ...
+                 && randomBits2(c) <= (2*a ...
+                        + 1)                    
+                        Cecoded(c) = inputAlphabet(b,a);
+                end               
+            end
+        end        
+    end     % decision algorithm
+
+    for c = 1:1:bitLength     
+        for b = 1:1:levelCounter^2 + 1
+            if Cecoded(c) == (b - 1)    
+                inputMatrix(b) = inputMatrix(b) + 1; 
+            end
+
+            for a = 1:1:levelCounter^2 + 1          
+                if Decoded(c) == (b - 1) && Cecoded(c) == (a - 1)                    
+                    probabilityMatrix(b,a) = probabilityMatrix(b,a) + 1;
+                end
+            end
+        end
+    end
 
 end
