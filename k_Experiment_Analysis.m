8447822b5f1f5f909aaa4640521b080b549b9a3f
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-11-09 10:56:41 +0000

    Cleaning Code ...

--- k_Experiment_Analysis.m
+++ k_Experiment_Analysis.m
@@ -18,11 +18,9 @@ leftoverTheoretical         = zeros(4, 11);
 leftoverExperimentalAverage = zeros(4, 11);
 leftoverExperimental        = zeros(12, 11);
 
-bitSequence                 = zeros(4, 22);
-bitRepetition               = zeros(4, 22);
+bitSequence                 = zeros(4, 22); bitRepetition   = zeros(4, 22);
 
-CmNoisedk                   = cell(4,1);
-kData                       = cell(4,1);
+CmNoisedk                   = cell(4,1);    kData            = cell(4,1);
 
 % Model Parameters --------------------------------------------------------
 
@@ -78,7 +76,9 @@ for kValue = 1:1:4      % Comparing theoretical and experimental values ---
 
 end
 
-p = cell2mat(kExperimentalData);
+% converting cell Data to array (because the loop is keep giving me errors)
+
+arraykExperimentalData = cell2mat(kExperimentalData);
 
 for t = 1:1:11
     
@@ -86,83 +86,36 @@ for t = 1:1:11
 
     for i = 1:1:4
 
+        % Generating the leftover values after flushing and its average
+
         leftoverTheoretical(i,t)            = CmNoisedk{i}(1,20*i + 40*i*(t-1));
-        leftoverExperimentalAverage(i,t)    = kData{i}(1,20*i + 40*i*(t-1));
+        leftoverExperimentalAverage(i,t)    = kData{i}(1,20*i + 40*i*(t-1))*10^9;
 
         for j = 1:1:3
             
-            leftoverExperimental(i*j,t)     = p(i,20*i + 40*i*(t-1));
+            leftoverExperimental(counter,t) = arraykExperimentalData(i*j,20*i + 40*i*(t-1))*10^9;
             
             counter = counter + 1;
+
         end
-    end
-   
+    end   
 end
 
-distance = 1:1:11;
-x3 = [1 2 3 4 5 7 8 9 10 11];
+repetitionCount = 1:1:11;               % number of repetitions done
 
 errBar  = [range([leftoverExperimental(1,:);  leftoverExperimental(2,:);  leftoverExperimental(3,:)]); ...
            range([leftoverExperimental(4,:);  leftoverExperimental(5,:);  leftoverExperimental(6,:)]); ...
            range([leftoverExperimental(7,:);  leftoverExperimental(8,:);  leftoverExperimental(9,:)]); ...
            range([leftoverExperimental(10,:); leftoverExperimental(11,:); leftoverExperimental(12,:)])]./2;
 
-%err_k_3(6) = [];
-%Leftover_E_3(6) =[];
-
 figure
-for i = 1:1:4
-plot(leftoverTheoretical(i,:),'Linewidth',2 )
-hold on
 
+for i = 1:1:4
+    plot(leftoverTheoretical(i,:),'Linewidth',2 );                             hold on;
+    errorbar(repetitionCount,leftoverExperimentalAverage(i,:),errBar(i),'o');  hold on;
 end
 
-errorbar(distance,Leftover_E_1,err_k_1,'o');
-hold on
-% Nice plot code ----------------------------------------------------------
-
-set(gca,'TickLabelInterpreter', 'latex');
-set(gca, ...
-  'Fontsize'    , 15        , ...
-  'TickDir'     , 'out'      , ...
-  'YGrid'       , 'on'      , ...
-  'GridLineStyle','--'      , ...
-  'TickLength'  , [.02 .02] , ...
-  'XMinorTick'  , 'on'      , ...
-  'YMinorTick'  , 'on'      , ...
-  'Box'         , 'off'      , ...
-  'LineWidth'   , 1.2         );
-
-% ------------------------------------------------------------------------- 
-ylabel('Leftover Signal [nA]','Interpreter','Latex')
-xlabel('Repetition','Interpreter','Latex')
-%title({'$D$ = 0.2 $\mathrm{cm^2/s}$, $u_x$ = 0.155 $\mathrm{cm/s}$, $x_d$ = 2.5 $\mathrm{cm}$'; '$ M_{k=1}$ = 0.6 $\mathrm{ng}$, $M_{k=2}$ = 0.9 $\mathrm{ng}$'; '$M_{k=3}$ = 1.2 $\mathrm{ng}$, $M_{k=4}$ = 1.9 $\mathrm{ng}$'},'Interpreter','Latex','Fontsize',16)
-
-set(legend('$k$ = 1','$k$ = 2','$k$ = 3','$k$ = 4'), ...
-           'Interpreter', 'Latex', ...
-           'Location', 'Northwest')
-
-xlim([1 10])
-
-% Correlation Values ------------------------------------------------------
-
-rho_k_1 = corrcoef(L_T_1,k1(1:length(L_T_1)));
-rho_k_2 = corrcoef(L_T_2,K_2(1:length(L_T_2)));
-rho_k_3 = corrcoef(L_T_3,K_3(1:length(L_T_3)));
-rho_k_4 = corrcoef(L_T_4,K_4(1:length(L_T_4)));
-
-% ============================ FUNCTIONS ==================================
-
-function kPlot(kData, CmNoisedk, kValue, diffusion, advectiveFlow, distance)
-
-    tickValues = zeros(1, 7);
-
-    figure
-    plot(cell2mat(kData(kValue,:))*10^9      ,'Linewidth', 2)
-    hold on
-    plot(cell2mat(CmNoisedk(kValue,:))   ,'Linewidth', 2)
-    
-    % Nice plot code ------------------------------------------------------
+% Nice plot code ---------------------------------------------------------
     
     set(gca, ...
       'TickLabelInterpreter'    , 'latex'       , ...
@@ -173,31 +126,70 @@ function kPlot(kData, CmNoisedk, kValue, diffusion, advectiveFlow, distance)
       'TickLength'              , [.02 .02]     , ...
       'GridLineStyle'           , '--'          , ...
       'YMinorTick'              , 'on'          , ...
-      'Box'                     , 'on'                  , ...
-      'XTick'                   , 0:20*kValue:420* kValue     ,...
-      'YTick'                   , 0:0.4:2       ,...
+      'Box'                     , 'on'          , ...
       'LineWidth'               , 1.5           );
+
+ % ------------------------------------------------------------------------
+
+xlabel('Repetition'             , 'Interpreter', 'Latex')
+ylabel('Leftover Signal [nA]'   , 'Interpreter', 'Latex')
+
+%title({'$D$ = 0.2 $\mathrm{cm^2/s}$, $u_x$ = 0.155 $\mathrm{cm/s}$, $x_d$ = 2.5 $\mathrm{cm}$'; '$ M_{k=1}$ = 0.6 $\mathrm{ng}$, $M_{k=2}$ = 0.9 $\mathrm{ng}$'; '$M_{k=3}$ = 1.2 $\mathrm{ng}$, $M_{k=4}$ = 1.9 $\mathrm{ng}$'},'Interpreter','Latex','Fontsize',16)
+
+set(legend('$k$ = 1', '$k$ = 2', '$k$ = 3', '$k$ = 4'), ...
+           'Interpreter', 'Latex', ...
+           'Location'   , 'Northwest')
+
+xlim([1 10])
+
+% Correlation Values ------------------------------------------------------
+
+for kValue = 1:1:4
+    correlationValues(kValue) = findCorrelation(cell2mat(CmNoisedk(kValue,:)), ...
+                                               (kData{kValue}(1:420*kValue)));
+end
+
+% ============================ FUNCTIONS ==================================
+
+function kPlot(kData, CmNoisedk, kValue, diffusion, advectiveFlow, distance)
+
+    tickValues = zeros(1, 7);
+
+    figure
+    plot(cell2mat(kData(kValue,:))*10^9  ,'Linewidth', 2);  hold on;
+    plot(cell2mat(CmNoisedk(kValue,:))   ,'Linewidth', 2);
+    
+    % Nice plot code ------------------------------------------------------
+    
+    set(gca, ...
+      'TickLabelInterpreter'    , 'latex'                       , ...
+      'Fontsize'                , 16                            , ...
+      'TickDir'                 , 'in'                          , ...
+      'XGrid'                   , 'on'                          , ...
+      'YGrid'                   , 'on'                          , ...
+      'TickLength'              , [.02 .02]                     , ...
+      'GridLineStyle'           , '--'                          , ...
+      'YMinorTick'              , 'on'                          , ...
+      'Box'                     , 'on'                          , ...
+      'XTick'                   , 0:20*kValue:420* kValue       , ...
+      'YTick'                   , 0:0.4:2                       , ...
+      'LineWidth'               , 1.5                           );
 
     % ---------------------------------------------------------------------
     
-    xlim([0 420*kValue]);
-    ylim([0 2])
+    xlim([0 420*kValue]);   ylim([0 2]);
     
     for n = 1:1:7
         tickValues(n) = 60*n*kValue;
     end
 
-    xticklabels({'0', '', '', tickValues(1), ...
-                      '', '', tickValues(2), ...
-                      '', '', tickValues(3), ...
-                      '', '', tickValues(4), ...
-                      '', '', tickValues(5), ...
-                      '', '', tickValues(6), ...
+    xticklabels({'0', '', '', tickValues(1), '', '', tickValues(2), ...
+                      '', '', tickValues(3), '', '', tickValues(4), ...
+                      '', '', tickValues(5), '', '', tickValues(6), ...
                       '', '', tickValues(7)})
 
-    ylabel('Signal Current [nA]','Interpreter','Latex')
-    xlabel('Transmission Time [s]','Interpreter','Latex')
-    
+    ylabel('Signal Current [nA]'    , 'Interpreter', 'Latex')
+    xlabel('Transmission Time [s]'  , 'Interpreter', 'Latex')
     
    % title({"$D$ = " + diffusion + " $\mathrm{cm^2/s}$, $u_x$ =  $\mathrm{cm/s}$, $x_d$ = " distance + " $\mathrm{cm}$";" $M$ = 1.2 $\mathrm{ng}$, $\rho$ = 0.94"},'Interpreter','Latex','Fontsize',16)
     
